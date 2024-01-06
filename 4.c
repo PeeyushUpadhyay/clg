42f60561a14166cb8d1148ab8288084af8c9efa4
PeeyushUpadhyay <peeyushupadhyay7@gmail.com>
2024-01-06 08:22:52 +0530

    d

---
+++ 4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+main(){
+    int i=1, j=1, n=5;
+    while(i<=n){
+        while(j<=n-i+1){
+            printf(" * ");
+            j++;
+        }
+        printf("\n");
+        i++;
+        j=1;
+    }
+    i=2;
+    j=1;
+    while(i<=n){
+        while(j<=i){
+            printf(" * ");
+            j++;
+        }
+        printf("\n");
+        i++;
+        j=1;
+    }
+}
